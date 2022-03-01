2ff0a80eb7add08f9a9efb3f00081747226f544c
Fabio Machado <fabio.a.machado@hotmail.com>
2022-03-01 16:32:05 -0300

    Criando projeto

---
+++ src/main/java/project/mentorship/projectmentorship/ProjectMentorshipApplication.java
@@ -0,0 +1,13 @@
+package project.mentorship.projectmentorship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMentorshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMentorshipApplication.class, args);
+	}
+
+}
